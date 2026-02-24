598cbd02ec1e898ea3a10ce1c83eaba94acbe179
Kashyap Ojha <kashyap.ojha24@gmail.com>
2026-02-24 21:02:02 +0530

    final commit

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -1,5 +1,5 @@
 // config.js
 // attach BASE_URL to window so all scripts can access it
 window.APP_CONFIG = {
-    BASE_URL: "http://localhost:3000"  // change per environment
+    BASE_URL: "http://localhost:3030"  // change per environment
 };
